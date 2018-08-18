d60f7af8c808961c9d981b70478ffabc5be4a5dd
Christopher Kok <ckok@purdue.edu>
2018-08-18 08:52:45 -0400

    Updated ImageCreator and added gitignore

--- ImageCreator.py
+++ ImageCreator.py
@@ -1,29 +1,37 @@
 from PIL import Image, ImageDraw, ImageFont
 
-W, H = (2400, 1000)
-img = Image.new('RGB', (W,H), color = (0, 0, 0))
+nameArray = ['Kelly','Maria']
 
-draw = ImageDraw.Draw(img)
+count = 1
+for x in nameArray:
+    bottomspace = 200
+    W, H = (2500, 1000 + bottomspace)
+    img = Image.new('RGB', (W,H), color = (0, 0, 0))
 
-txt = "Chris" # text to be generated
-fontsize = 1  # starting font size
+    draw = ImageDraw.Draw(img)
 
-# portion of image width you want text width to be
-img_fraction = 0.70
+    txt = x # text to be generated
+    if (len(x) == 3) or (len(x) == 4):
+        txt = ' ' + x + ' '
+    fontsize = 1  # starting font size
 
-font = ImageFont.truetype("arial.ttf", fontsize)
-while font.getsize(txt)[0] < img_fraction*img.size[0]:
-    # iterate until the text size is just larger than the criteria
-    fontsize += 1
+    # portion of image width you want text width to be
+    img_fraction = 0.60
+
+    font = ImageFont.truetype("arial.ttf", fontsize)
+    while font.getsize(txt)[0] < img_fraction*img.size[0]:
+        # iterate until the text size is just larger than the criteria
+        fontsize += 1
+        font = ImageFont.truetype("arial.ttf", fontsize)
+
+    # optionally de-increment to be sure it is less than criteria
+    fontsize -= 1
     font = ImageFont.truetype("arial.ttf", fontsize)
 
-# optionally de-increment to be sure it is less than criteria
-fontsize -= 1
-font = ImageFont.truetype("arial.ttf", fontsize)
+    w, h = draw.textsize(txt, font=font)
 
-w, h = draw.textsize(txt, font=font)
-
-# print ('final font size',fontsize)
-# print ('dimensions', W, H, w, h )
-draw.text(((W-w)/2,(H-h)/2), txt, font=font) # put the text on the image
-img.save('chris.png') # save it
+    # print ('final font size',fontsize)
+    # print ('dimensions', W, H, w, h )
+    draw.text(((W-w)/2,(H-h-bottomspace)/2), txt, font=font) # put the text on the image
+    img.save('names/' + str(count) + '.png') # save it
+    count = count + 1
